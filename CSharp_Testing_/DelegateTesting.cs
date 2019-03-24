3727606e734534c65f130658237e3a03f49beff9
fizztheturtle <kabeerf12@outlook.com>
2019-03-24 16:28:40 +0000

    testing delegates

---
+++ CSharp_Testing_/DelegateTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Testing_
+{
+    class DelegateTesting
+    {
+        public delegate void Del(string message);
+
+        public static void DelegateMethod(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+
+       
+
+        public void MethodWithCallback(int param1, int param2, Del callback)
+        {
+            callback("The number is: " + (param1 + param2).ToString());
+        }
+
+    }
+}
--- CSharp_Testing_/Program.cs
+++ CSharp_Testing_/Program.cs
@@ -6,11 +6,31 @@ namespace CSharp_Testing_
 
         public static void Main(string[] args)
         {
-            //var numClass = new Numbers_Class();
-            
-            //Numbers_Class.Fibbonaci_Math();
-            TestingStatements.Switch_Test();
+            var DelegateTestingClass = new DelegateTesting();
 
+            //Numbers_Class.Fibbonaci_Math();
+
+            //TestingStatements.Switch_Test();
+            //TestingStatements.ListTest();
+            //TestingStatements.IterateSpanExample();
+            //TestingStatements.Spanesting_0();
+            //TestingStatements.Spanesting_1();
+            //TestingStatements.Spanesting_2();
+            //TestingStatements.Spanesting_3();
+            // Instantiate the delegate.
+          
+            DelegateTesting.Del handler = DelegateTesting.DelegateMethod;
+
+            // Call the delegate.
+            handler("Hello World");
+
+            Console.WriteLine("Enter first Value:");
+            string input0 = Console.ReadLine();
+            int inputInt0 = Int32.Parse(input0);
+            Console.WriteLine("Enter Second Value:");
+            string input1 = Console.ReadLine();
+            int inputInt1 = Int32.Parse(input1);
+            DelegateTestingClass.MethodWithCallback(inputInt0, inputInt1, handler);
             Console.ReadLine();
         }
 
